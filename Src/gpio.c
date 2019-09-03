fb2f4337cbde33192c7ca85274a5f3f818bc9f26
QuangNguyen <ntdquang1412@gmail.com>
2019-09-03 22:20:26 +0700

    Reconfigure GPIOB

--- Src/gpio.c
+++ Src/gpio.c
@@ -97,12 +97,12 @@ void GPIO_Init()
   GPIO_InitTypeDef GPIO_InitStruct = {0};
 
   // B1 Config
-  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1;
+  GPIO_InitStruct.Pin = GPIO_PIN_1;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0|GPIO_PIN_1, GPIO_PIN_SET);
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_SET);
 }
 /* USER CODE BEGIN 2 */
 
